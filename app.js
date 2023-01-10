d66e9fbfed461a6d7a471a6144c58307d8c728d5
payalpagaria <pagariapayal67@gmail.com>
2023-01-10 19:29:06 +0530

    mailchimp api is used

--- app.js
+++ app.js
@@ -2,10 +2,41 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const request=require('request');
 const path=require('path');
+const https=require('https');
+const { url } = require('inspector');
+const { response } = require('express');
+const dotenv=require('dotenv').config();
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}))
 app.post("/",(req,res)=>{
-    console.log(req.body.fname)
+   var firstName=req.body.fname
+   var lastName=req.body.lname
+   var email =req.body.email
+   var data={
+        members:[
+            {
+                email_address:email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME:firstName,
+                    LNAME:lastName
+                }
+            }
+        ]
+   };
+   const jsonData=JSON.stringify(data);
+   const url=`https://${process.env.DC}.api.mailchimp.com/3.0/lists/${process.env.UID}`;
+   const options={
+    method:"POST",
+    auth:`payal:${process.env.APIKEY}`
+   }
+   const request=https.request(url,options,(response)=>{
+        response.on("data",function(data){
+            console.log(JSON.parse(data))
+        })
+   })
+   request.write(jsonData);
+   request.end();
 })
 const port=3000;
 app.use(express.static("public"))
@@ -17,3 +48,7 @@ app.get("/",(req,res)=>
 app.listen(port,()=>{
     console.log(`The server is started on port ${port}`);
 })
+
+
+
+
